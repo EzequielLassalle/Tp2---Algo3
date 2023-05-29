ea9f3e8d88a5a6ffe176dd33dc1ba52eecc59cbe
gonzalo912 <golmos@fi.uba.ar>
2023-05-29 17:34:16 -0300

    creacion test para entrega 1

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/Entrega1Test.java
@@ -0,0 +1,67 @@
+package edu.fiuba.algo3.entrega_1;
+
+import edu.fiuba.algo3.modelo.Message;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Entrega1Test {
+    @Test
+    public void test01EmpiezaPartidaYJugadorTieneVidaYCreditosCorrespondientes() {
+
+    }
+
+    @Test
+    public void test02TiempoConstruccionDeDefensasEsElCorrespondienteYSonUtilizables() {
+
+    }
+
+    @Test
+    public void test03VerificacionCreditosParaConstruir() {
+
+    }
+
+    @Test
+    public void test04SeVerificaQueSoloSePuedeConstruirSobreTierra() {
+
+    }
+
+    @Test
+    public void test05SeVerificaQueLasDefensasAtaquenDentroDelRangoEsperado() {
+
+    }
+
+    @Test
+    public void test06EnemigosRecibenElDañoEsperado() {
+
+    }
+
+    @Test
+    public void test07EnemigosSeMuevenSoloPorLaParcelaAutorizada() {
+
+    }
+
+    @Test
+    public void test08SeDestruyeUnidadEnemigaYJugadorCobraCreditosCorrespondientes() {
+
+    }
+
+    @Test
+    public void test09PasaUnTurnoYSeVerificaQueLasUnidadesEnemigasSeHayanMovidoSegunVelocidad() {
+
+    }
+
+    @Test
+    public void test10SeEliminanTodosLosEnemigosYElJugadorGanaLaPartida() {
+
+    }
+
+    @Test
+    public void test11NoSeEliminanLasUnidadesEnemigasYElJugadorNoMuereEntoncesGana(){
+
+    }
+
+    @Test
+    public void test12UnidadesEnemigasLleganALaMetaYMatanAlJugadorEntoncesPierde(){
+
+    }
+}
