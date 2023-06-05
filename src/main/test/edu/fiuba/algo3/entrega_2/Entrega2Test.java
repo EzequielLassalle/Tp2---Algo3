387525fe0971752e51969ff749179992235ba028
EzequielLassalle <ezelassalle9@gmail.com>
2023-06-05 17:24:15 -0300

    Modelo1

---
+++ src/main/test/edu/fiuba/algo3/entrega_2/Entrega2Test.java
@@ -0,0 +1,63 @@
+package edu.fiuba.algo3.entrega_2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import edu.fiuba.algo3.modelo.*;
+
+public class Entrega2Test{
+    @Test
+    public void test13FormatoJSONdeEnemigosEsElCorrecto() {
+
+ 
+
+    }
+
+    public void test14FormatoJSONdeMapaEsElCorrecto() {
+
+ 
+
+    }
+
+    public void test15LecturaYconversionDeJSONdeEnemigos() {
+
+ 
+
+    }
+
+    public void test16LecturaYconversiondeJSONMapa() {
+
+ 
+
+    }
+
+    public void test17JuegoBienCreado() {
+
+ 
+
+    }
+
+    public void test18JugadorJuegaYganaPartida() {
+
+ 
+
+    }
+    
+    public void test19JugadorJuegaYpierdePartida() {
+
+ 
+
+    }
+
+    public void test20VerificarSistemaLogParaEntrega3() {
+
+ 
+
+    }
+
+
+
+}
+
