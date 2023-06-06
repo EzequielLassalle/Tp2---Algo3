de016c476d3158cb11c17025d365092c27b4d7c1
EzequielLassalle <ezelassalle9@gmail.com>
2023-06-06 13:00:14 -0300

    Json simple andando

--- src/main/java/edu/fiuba/algo3/modelo/MapaParser.java
+++ src/main/java/edu/fiuba/algo3/modelo/MapaParser.java
@@ -1,9 +1,9 @@
 package edu.fiuba.algo3.modelo;
 
-///import com.json.simple.JSONObject;
-///import org.json.simple.JSONArray;
-///import org.json.simple.parser.JSONParser;
-////import org.json.simple.parser.ParseException;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 public class MapaParser {
