9e824849a283398fc9cb3a5ad6799223d863687d
gonzalo912 <golmos@fi.uba.ar>
2023-06-07 18:39:40 -0300

    modificado MapaParser

--- src/main/java/edu/fiuba/algo3/modelo/MapaParser.java
+++ src/main/java/edu/fiuba/algo3/modelo/MapaParser.java
@@ -18,44 +18,61 @@ import org.json.simple.parser.ParseException;
 
 
 public class MapaParser {
-
+    final int SIZE = 15;
     private String path;
 
     public MapaParser(String p) {
         this.path = p;
     }
 
-    public List parsearMapa() throws IOException, ParseException {
+    private JSONObject setJSON()throws IOException, ParseException{
         JSONParser parser = new JSONParser();
         FileReader reader = new FileReader(this.path);
         Object obj = parser.parse(reader);
         JSONObject jsonObj = (JSONObject) obj;
-        JSONObject map = (JSONObject) jsonObj.get("Mapa");
-        List lines = new ArrayList();
-        for (int i = 0; i < 15; i++) {
-            JSONArray line = (JSONArray) map.get(Integer.toString(i + 1));
-            List parsedLine = new ArrayList();
-            for (int x = 0; x < 15; x++) {
-                parsedLine.add(line.get(x));
-            }
-            lines.add(parsedLine);
-        }
-        return lines;
+        reader.close();
+        return jsonObj;
     }
 
-    public void formatoCorrecto() throws IOException, ParseException, JSONVacio, FormatoJSONInvalido{
-        JSONParser parser = new JSONParser();
-        FileReader reader = new FileReader(this.path);
-        Object obj = parser.parse(reader);
-        JSONObject jsonObj = (JSONObject) obj;
+    public Casillero[][] parsearMapa() throws IOException, ParseException {
+        JSONObject jsonObj = setJSON();
+        JSONObject map = (JSONObject) jsonObj.get("Mapa");
+        Casillero[][] mapa = new Casillero[SIZE][SIZE];
+        for (int y = 0; y < SIZE; y++) {
+            JSONArray line = (JSONArray) map.get(Integer.toString(y + 1));
+            for (int x = 0; x < SIZE; x++) {
+                mapa[x][y] = asignar((String)line.get(x), x, y);
+            }
+        }
+        return mapa;
+    }
+
+    private Casillero asignar(String s, int x, int y){
+        Casillero c = null;
+        switch (s) {
+            case "Tierra":
+                c = new Tierra(x, y);
+                break;
+            case "Rocoso":
+                c = new Rocosa(x, y);
+                break;
+            case "Pasarela":
+                c = new Pasarela(x, y);
+                break;
+        }
+        return c;
+    }
+
+    public void formatoCorrecto() throws IOException, ParseException, JSONVacio, FormatoJSONInvalido{s
+        JSONObject jsonObj = setJSON();
         if(jsonObj.isEmpty()) {
             throw new JSONVacio();
         }
         try{
             JSONObject map = (JSONObject) jsonObj.get("Mapa");
-            for (int i = 0; i < 15; i++) {
+            for (int i = 0; i < SIZE; i++) {
                 JSONArray line = (JSONArray) map.get(Integer.toString(i + 1));
-                for (int x = 0; x < 15; x++) {
+                for (int x = 0; x < SIZE; x++) {
                     String s = (String)line.get(x);
                     if(!s.equals("Tierra") && !s.equals("Rocoso") && !s.equals("Pasarela")){
                         throw new FormatoJSONInvalido();
--- src/main/test/edu/fiuba/algo3/entrega_2/Entrega2Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/Entrega2Test.java
@@ -31,7 +31,7 @@ public class Entrega2Test{
 
     public void test15LecturaYconversionDeJSONdeEnemigos() {
 
- 
+
 
     }
 
