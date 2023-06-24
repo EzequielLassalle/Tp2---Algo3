e23893aafd0873910eb743c9548a1c0ca69b9fe0
EzequielLassalle <ezelassalle9@gmail.com>
2023-06-24 15:42:45 -0300

    Imagenes

--- src/main/java/edu/fiuba/algo3/vistas/VistaArania.java
+++ src/main/java/edu/fiuba/algo3/vistas/VistaArania.java
@@ -1,9 +1,15 @@
 package edu.fiuba.algo3.vistas;
 
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
-public class VistaArania extends Rectangle {
-
-    
-    
+public class VistaArania extends Rectangle{
+    private Image image = new Image("src/main/java/edu/fiuba/algo3/vistas/images/hormiga.png");
+    public VistaArania(){
+        this.setFill(new ImagePattern(this.image));
+    }
 }
