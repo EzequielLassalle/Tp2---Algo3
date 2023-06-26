0aa0205c662c1d5fd71c0a371ca862920cacb04c
EzequielLassalle <ezelassalle9@gmail.com>
2023-06-26 11:17:37 -0300

    PruebaTest

---
+++ src/main/test/edu/fiuba/algo3/Tests/AraniaTest.java
@@ -0,0 +1,63 @@
+package edu.fiuba.algo3.Tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import edu.fiuba.algo3.modelo.*;
+
+public class AraniaTest {
+
+    @Test
+    public void test01AraniaSeCreaBien() {
+
+        Arania arania = new Arania();
+
+        assertEquals(arania, new Arania());
+
+
+    }    
+
+    @Test
+    public void test02AraniaSeMueveBien(){
+
+        Arania arania = new Arania();
+
+        Pasarela pasarela1 = new Pasarela(0, 0);
+        Pasarela pasarela2 = new Pasarela(1,0);
+        Pasarela pasararela3 = new Pasarela(2,0);
+
+
+        pasarela1.establecerSiguiente(pasarela2);
+        pasarela2.establecerSiguiente(pasararela3);
+
+        assertTrue(arania.mover(pasarela1) == pasararela3);
+
+
+
+    }
+
+    @Test
+    
+    public void test02AraniaRelantizadaSeMueveBien(){
+
+        Arania arania = new Arania();
+
+        Pasarela pasarela1 = new Pasarela(0, 0);
+        Pasarela pasarela2 = new Pasarela(1,0);
+    
+
+
+        pasarela1.establecerSiguiente(pasarela2);
+        
+
+        assertTrue(arania.moverRelantizado(pasarela1) == pasarela2);
+
+
+
+    }
+
+
+    
+}
---
+++ src/main/test/edu/fiuba/algo3/Tests/JuegoTest.java
@@ -0,0 +1,5 @@
+package edu.fiuba.algo3.Tests;
+
+public class JuegoTest {
+    
+}
