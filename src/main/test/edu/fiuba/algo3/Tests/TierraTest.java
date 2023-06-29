9cacb541e69cfca9a66d3281e74ac0f087b9b1a5
EzequielLassalle <ezelassalle9@gmail.com>
2023-06-29 00:29:52 -0300

    +TestsTierra

--- src/main/test/edu/fiuba/algo3/Tests/TierraTest.java
+++ src/main/test/edu/fiuba/algo3/Tests/TierraTest.java
@@ -1,5 +1,47 @@
 package edu.fiuba.algo3.Tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import edu.fiuba.algo3.modelo.*;
+
+
+
+
 public class TierraTest {
-    
+
+    @Test
+    public void Tierra01SeEstableceSinDefensa(){
+
+        Tierra tierra = new Tierra(1,1);
+
+        assertTrue(!tierra.hayDefensa());
+
+        assertTrue(tierra.vacio());
+
+
+
+    }
+
+    @Test
+        public void Tierra02EstableceSinDefensaYataca(){
+
+        Tierra tierra = new Tierra(1,1);
+
+        tierra.establecerDefensa(new TorreBlanca());
+
+        tierra.pasarTurno(new Casillero(0, 0));
+
+        assertTrue(tierra.hayDefensa());
+
+        tierra.eliminarDefensa();
+
+        assertTrue(!tierra.hayDefensa());
+
+    }
+
+
 }
+
